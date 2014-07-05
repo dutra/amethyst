96f8afffcaf31116931516dfc187d9d3687e75c4
Isaque Dutra <dutra@mit.edu>
2014-07-05 16:48:48 -0400

    Added dynamic album system from flickr account

--- app/assets/javascripts/albums.js
+++ app/assets/javascripts/albums.js
@@ -1,8 +1,26 @@
 
-$(document).ready(function() {
-    $(".item-hover").hover(function() {
-	$(this).stop().fadeTo(500, 0.6);
-    },function() {
-	$(this).stop().fadeTo(500, 1);
+// $(document).ready(function() {
+//     $(".item-hover").hover(function() {
+// 	$(this).stop().fadeTo(500, 0.6);
+//     },function() {
+// 	$(this).stop().fadeTo(500, 1);
+//     });
+// });
+
+$(document).ready(function () {
+    $("#gallery").nanoGallery({
+	kind : 'flickr',
+	userID : '85128398@N05',
+	locationHash: true,
+	colorScheme : 'none',
+	thumbnailHoverEffect : 'labelAppear75,borderDarker',
+	theme : 'clean',
+	i18n : {'thumbnailImageDescription':'View Photo', 'thumbnailAlbumDescription':'Open Album'},
+	thumbnailLabel : {hideIcons: true, display:true,position:'overImageOnMiddle', align: 'center'},
+	thumbnailWidth : 'auto',
+	thumbnailHeight : 250,
+	galleryToolbarWidthAligned: true,
+	galleryToolbarHideIcons: true
     });
 });
+
--- config/application.rb
+++ config/application.rb
@@ -58,5 +58,9 @@ module Amethyst
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    # add app/assets/fonts to the asset path
+    config.assets.paths << Rails.root.join("app", "assets", "fonts")
+
   end
 end
